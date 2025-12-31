9fa0509bcb85958568dbad0da4744f4ef7b16e05
NKDShinKu <2010182879@qq.com>
2025-12-31 18:15:41 +0800

    chore: 插件安装与配置

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div class="w-1 h-2"></div>
+  <div class="text-white px-4 bg-blue-500 rounded py-2">
+    Hello
+  </div>
+
 </template>
 
 <style scoped></style>
