f8e572cc6147d251221a9fed7e83cd1ca38d06de
NKDShinKu <2010182879@qq.com>
2026-06-29 23:45:42 +0800

    chore: 清理旧版 skills 文件结构

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// AppLayout 将在 Phase C 实现，目前先用 RouterView 占位
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <div class="w-1 h-2"></div>
-  <div class="text-white px-4 bg-blue-500 rounded py-2">
-    Hello
-  </div>
-
+  <RouterView />
 </template>
 
-<style scoped></style>
